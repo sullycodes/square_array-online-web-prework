9f755792b4ef6649dc606259a557d92095789190
Daniel Sullivan <dansullivan19@gmail.com>
2019-04-02 15:49:37 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,12 @@
 def square_array(array)
+  # your code here
+  array = [1, 2, 3, 4, 5]
+  new_numbers = []
   
   array.each do |number|
-  new_array = number * number
-  puts new_array
+  new_numbers.collect = number * number
   end
+  puts new_numbers
 end
 
-square_array(array = [1,2,3])
+square_array()
